5672af7f4e57c4de7973aae6e88aeb6c93466136
Melvin_Homefront <melvinkusters0@gmail.com>
2018-04-09 02:07:08 +0200

    First commit.

---
+++ TorrentCheck/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TorrentCheck.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
